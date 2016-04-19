3e44a690b84e641cb418a62ee8303914215994b5
Izzet Mustafaiev <webdizz@gmail.com>
2016-04-19 23:46:34 +0300

    Remove Hystrix dashboard

--- src/main/java/name/webdizz/fabric8/ci/cd/customer/Application.java
+++ src/main/java/name/webdizz/fabric8/ci/cd/customer/Application.java
@@ -3,11 +3,9 @@ package name.webdizz.fabric8.ci.cd.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
