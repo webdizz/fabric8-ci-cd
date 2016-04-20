48e45fca1eb7f3eac7fde11180a2522066f95277
Izzet Mustafaiev <webdizz@gmail.com>
2016-04-20 22:06:11 +0300

    Add logging to demonstrate central logs aggregation

--- src/main/java/name/webdizz/fabric8/ci/cd/ping/DynamicPingController.java
+++ src/main/java/name/webdizz/fabric8/ci/cd/ping/DynamicPingController.java
@@ -1,11 +1,14 @@
 package name.webdizz.fabric8.ci.cd.ping;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DynamicPingController {
 
@@ -14,6 +17,8 @@ public class DynamicPingController {
 
     @RequestMapping("/ping")
     public String ping() {
-        return String.format("Hello, I'm a Pinger @%s:%s of %s generation!", environment.getProperty("hostname"), environment.getProperty("kubernetes.namespace"), environment.getProperty("app.version"));
+        String message = String.format("Hello, I'm a Pinger @%s:%s of %s generation!", environment.getProperty("hostname"), environment.getProperty("kubernetes.namespace"), environment.getProperty("app.version"));
+        log.info(message);
+        return message;
     }
 }
