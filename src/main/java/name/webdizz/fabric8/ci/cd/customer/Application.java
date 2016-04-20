c5d8eff90081a8171d9c931c8fbf2a9e8056b511
Izzet Mustafaiev <webdizz@gmail.com>
2016-04-20 11:25:09 +0300

    Add dynamic behaviour to application

--- src/main/java/name/webdizz/fabric8/ci/cd/customer/Application.java
+++ src/main/java/name/webdizz/fabric8/ci/cd/customer/Application.java
@@ -2,10 +2,8 @@ package name.webdizz.fabric8.ci.cd.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/name/webdizz/fabric8/ci/cd/customer/DynamicPingController.java
@@ -0,0 +1,19 @@
+package name.webdizz.fabric8.ci.cd.customer;
+
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DynamicPingController {
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return String.format("Hello, I'm a Pinger @%s of %s generation!", environment.getProperty("kubernetes.namespace"), environment.getProperty("app.version"));
+    }
+}
